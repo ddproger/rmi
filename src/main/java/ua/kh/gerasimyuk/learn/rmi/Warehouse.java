76f40e0dcf9f683bcdb3e86e88576429ca3e4127
dgerasymiuk <d.gerasymiuk@gmail.com>
2017-01-16 19:21:50 +0200

    Add Warehouse interface

---
+++ src/main/java/ua/kh/gerasimyuk/learn/rmi/Warehouse.java
@@ -0,0 +1,11 @@
+package ua.kh.gerasimyuk.learn.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by gerasymiuk on 16.01.17.
+ */
+public interface Warehouse extends Remote {
+    double getPrice(String description) throws RemoteException;
+}
